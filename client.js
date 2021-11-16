250ca8cd05eb3a0da622837d20381269f107fcd6
Nico Dann <nicodann@gmail.com>
2021-11-16 16:47:07 +0000

    created client.js and moved connect function there from play.js

---
+++ client.js
@@ -0,0 +1,16 @@
+const connect = function () {
+
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
