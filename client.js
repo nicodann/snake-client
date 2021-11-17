18b8f51e6710dd7d0275310c2a0963f7572824e2
Nico Dann <nicodann@gmail.com>
2021-11-17 16:11:03 +0000

    experimented with automatic move commands

--- client.js
+++ client.js
@@ -15,9 +15,12 @@ const connect = function () {
 
   conn.on("connect", () => {
     console.log("Connection successful!");
-  })
+    conn.write("Name: BOO");
+    // setTimeout(() => {conn.write("Move: up")}, 50);
+    // setTimeout(() => {conn.write("Move: up")}, 100);
+    // setInterval(() => {conn.write("Move: up")}, 50);
+  });
 
-  conn.write("Name: BOO")
 
   return conn;
 };
