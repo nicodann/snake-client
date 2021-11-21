4e046d1ad517eb7a8e6cbf44bc740d998d6f1249
Nico Dann <nicodann@gmail.com>
2021-11-21 01:24:38 +0000

    Make README.md, re-factor constants, fix snake movement with intervalID and clearInterval

--- constants.js
+++ constants.js
@@ -1,9 +1,15 @@
 const IP = "localhost";
 const PORT = 50541;
-const MOVE_UP_KEY = 'w';
-const MOVE_LEFT_KEY = 'a';
-const MOVE_DOWN_KEY = 's';
-const MOVE_RIGHT_KEY = 'd';
+const directions = {
+  'w': "Move: up",
+  'a': "Move: left",
+  's': "Move: down",
+  'd': "Move: right"
+}
+// const MOVE_UP_KEY = 'w';
+// const MOVE_LEFT_KEY = 'a';
+// const MOVE_DOWN_KEY = 's';
+// const MOVE_RIGHT_KEY = 'd';
 const messages = {
   '1': "Say: I'm a snake",
   '2': "Say: Hisssssss!",
@@ -14,9 +20,6 @@ const messages = {
 module.exports = { 
   IP,
   PORT, 
-  MOVE_UP_KEY,
-  MOVE_LEFT_KEY,
-  MOVE_DOWN_KEY, 
-  MOVE_RIGHT_KEY,
+  directions,
   messages
 };
--- input.js
+++ input.js
@@ -1,43 +1,55 @@
 const {
-  MOVE_UP_KEY,
-  MOVE_LEFT_KEY,
-  MOVE_DOWN_KEY, 
-  MOVE_RIGHT_KEY,
+  directions,
   messages
 } = require('./constants');
 
 // setup interface to handle user input from stdin
 let connection;
+let intervalID;
 
 const handleUserInput =  (key) => {
+  clearInterval(intervalID);
+  intervalID = null;
+  
   if (key === '\u0003') {
     process.exit();
   }  else if (messages[key]) {
     conn.write(messages[key]);
-  } else if (key === MOVE_UP_KEY) {
-      conn.write("Move: up");
-  } else if (key === MOVE_LEFT_KEY) {
-    conn.write("Move: left");
-  } else if (key === MOVE_DOWN_KEY) {
-    conn.write("Move: down");
-  } else if (key === MOVE_RIGHT_KEY) {
-    conn.write("Move: right");
-  } else if (messages[key]) {
-    conn.write(messages[key]);
+  } else if (directions[key]) {
+    if (!intervalID) {
+      intervalID = setInterval(() => conn.write(directions[key]), 50);
+    }
   }
-  
-  // while (key === MOVE_UP_KEY) {
-  //   setInterval(conn.write("Move: up"),1000);
+
+  // else if (key === MOVE_UP_KEY) {
+  //   clearInterval(intervalID);
+  //   intervalID = setInterval(() => conn.write("Move: up"), 500);
+  // } else if (key === MOVE_LEFT_KEY) {
+  //   clearInterval(intervalID);
+  //   intervalID = setInterval(() => conn.write("Move: left"), 500);
+  // } else if (key === MOVE_DOWN_KEY) {
+  //   clearInterval(intervalID);
+  //   intervalID = setInterval(() => conn.write("Move: down"), 500);
+  // } else if (key === MOVE_RIGHT_KEY) {
+  //   clearInterval(intervalID);
+  //   intervalID = setInterval(() => conn.write("Move: right"), 500);
   // } 
-  // while (key === MOVE_LEFT_KEY) {
+  
+  // while (button === MOVE_UP_KEY) {
+  //   // setInterval(conn.write("Move: up"),1000);
+  //   conn.write("Move: up")
+  //   conn.write("Move: up")
+  //   conn.write("Move: up")
+  // } 
+  // while (button === MOVE_LEFT_KEY) {
   //   setInterval(conn.write("Move: left"),1000);
   // } 
   
-  // while (key === MOVE_DOWN_KEY) {
+  // while (button === MOVE_DOWN_KEY) {
   //   setInterval(conn.write("Move: down"),1000);
   // } 
   
-  // while (key === MOVE_RIGHT_KEY) {
+  // while (button === MOVE_RIGHT_KEY) {
   //   setInterval(conn.write("Move: right"), 1000);
   // } 
   
